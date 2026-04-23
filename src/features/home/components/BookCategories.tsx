00475a9a60f199637c501b1a214490394828f965
Nicholas Odhiambo <odhiambonicolas.no@gmail.com>
2026-04-23 17:29:06 +0300

    catgery section

--- src/features/home/components/BookCategories.tsx
+++ src/features/home/components/BookCategories.tsx
@@ -1,16 +1,28 @@
-import { Box, Container, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
 export const BookCategories = () => {
   return (
     <>
-      <Box>
-        <Container maxW={"6xl"} py={{ base: 10, md: 22 }}>
-          <Box>
-            <Text display={"flex"} justifyContent={"right"}>
-              Book Category
+      <Box w="100%" py={8}>
+        <Box maxW="6xl" mx="auto" px={6}>
+          <Flex justify="space-between" align="center" mb={4}>
+            <Heading size="md">Book Categories</Heading>
+            <Text fontSize="sm" cursor="pointer">
+              See all →
             </Text>
-          </Box>
-        </Container>
+          </Flex>
+
+          {/* Carousel */}
+          <Flex
+            gap={4}
+            overflowX="auto"
+            pb={2}
+            css={{
+              scrollBehavior: "smooth",
+              "&::-webkit-scrollbar": { display: "none" },
+            }}
+          ></Flex>
+        </Box>
       </Box>
     </>
   );
